cada95f4d79cc7545c626e895c45e0a1a41070f8
Patrick Roche <rochep63@gmail.com>
2020-01-30 19:54:00 -0500

    working

--- src/suas_planning/src/GlobalPlannerNode.cpp
+++ src/suas_planning/src/GlobalPlannerNode.cpp
@@ -1,20 +1,21 @@
-#include "GlobalPlannerNode.hpp"
-#include "GlobalWaypointPlanner.hpp"
-#include "PlanningPoints.hpp"
+//#include "GlobalPlannerNode.hpp"
+//#include "GlobalWaypointPlanner.hpp"
+//#include "PlanningPoints.hpp"
 
 #include <ros/ros.h>
+#include <ros/console.h>
 #include <signal.h>
 
-namespace suas_planning
-{
+//namespace suas_planning
+//{
     
 int main(int argc, char** argv)
 {
     //GlobalWaypointPlanner gwp;
-    MapMetaInfo mmi;
-    Obstacle ob;
-    CircularObstacle cob;
-    Waypoint wp;
+    //MapMetaInfo mmi;
+    //Obstacle ob;
+    //CircularObstacle cob;
+    //Waypoint wp;
 
     // TODO
     // ros node
@@ -22,9 +23,13 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "global_planner_node");
     ros::NodeHandle nh;
 
+    ROS_DEBUG_ONCE("hello suas");
+
+    std::cout << "hello suas" << std::endl;
+
     ros::spin();
 
     return 0;
 }
 
-} // namespace suas_planning
+//} // namespace suas_planning
