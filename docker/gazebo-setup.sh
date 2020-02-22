1fb63433e39b2e921f8b603b4d1758b7aeff7f77
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2020-02-22 14:14:54 -0500

    Export SVGA_VGA10 to fix io_ctl error in Virtualbox

--- docker/gazebo-setup.sh
+++ docker/gazebo-setup.sh
@@ -8,3 +8,4 @@ export GAZEBO_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/gazebo-9/plugins:$(pwd)/buil
 export GAZEBO_MODEL_PATH=/usr/share/gazebo-9/models:$(pwd)/gazebo_models:${GAZEBO_MODEL_PATH}:
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/lib/x86_64-linux-gnu/gazebo-9/plugins
 export OGRE_RESOURCE_PATH=/usr/lib/x86_64-linux-gnu/OGRE-1.9.0
+export SVGA_VGA10=0
